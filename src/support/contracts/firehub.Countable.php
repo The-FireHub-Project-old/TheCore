2a4234ae4eb4e307a580ab21122cec1789d2093a
Riddick <danijel.galic@live.com>
2024-01-15 13:11:50 +0100

    Create Countable contract (#10)

---
+++ src/support/contracts/firehub.Countable.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2024 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Core\Support\Contracts;
+
+use Countable as InternalCountable;
+
+/**
+ * ### Countable contract
+ *
+ * Classes implementing Countable can be used with the count() function.
+ * @since 1.0.0
+ */
+interface Countable extends InternalCountable {
+
+    /**
+     * ### Count elements of an object
+     * @since 1.0.0
+     *
+     * @return int Number of elements of an object.
+     * @phpstan-return non-negative-int
+     */
+    public function count ():int;
+
+}
