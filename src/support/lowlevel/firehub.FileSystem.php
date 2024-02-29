933d7815c5144fc45b68acfd269eef97d6512f50
Riddick <danijel.galic@live.com>
2024-02-29 15:06:54 +0100

    Refactor pathInfo method (#33)

--- src/support/lowlevel/firehub.FileSystem.php
+++ src/support/lowlevel/firehub.FileSystem.php
@@ -255,11 +255,12 @@ class FileSystem {
 
         $path_info = pathinfo($path);
 
-        if (!isset($path_info['dirname'])) $path_info['dirname'] = false;
-        if (!isset($path_info['extension'])) $path_info['extension'] = false;
-        if (!isset($path_info['filename'])) $path_info['filename'] = false;
-
-        return $path_info;
+        return [
+            'dirname' => $path_info['dirname'] ?? false,
+            'basename' => $path_info['basename'],
+            'extension' => $path_info['extension'] ?? false,
+            'filename' => $path_info['filename'] ?? false,
+        ];
 
     }
 
