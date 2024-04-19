3e9fa6d56c587e0342f6d2ec2e4e6007bed95da0
Riddick <danijel.galic@live.com>
2024-04-19 09:46:16 +0200

    Add doc (#33)

--- src/support/firehub.IStr.php
+++ src/support/firehub.IStr.php
@@ -40,6 +40,7 @@ final class IStr extends Str {
      * @since 1.0.0
      *
      * @uses \FireHub\Core\Support\Enums\String\Expression\Modifier::MULTIBYTE As default modifier.
+     * @uses \FireHub\Core\Support\Enums\String\Expression\Modifier::CASELESS As default modifier.
      * @uses \FireHub\Core\Support\Strings\Expression As return.
      */
     public function expression ():Expression {
